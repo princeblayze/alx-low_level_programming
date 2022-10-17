919654d3c1b56f1ce02566d43dd70fb7940342cd
princeblayze <princeblayze@gmail.com>
2022-10-16 18:25:44 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,13 @@
 #!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+
